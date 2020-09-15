35465a050027b999ddbcc4a5b82f2b4dd12b825e
pardeep-mern <pardeep889@hotmail.com>
2020-09-15 20:05:52 +0530

    fix the deployment script

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+
+const compiledFactory  = require("./build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
   process.env.MNEMONIC,
@@ -11,10 +12,13 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({
-    data: bytecode,
-    arguments: ['Pardeep'],
-  }).send({gas: '1000000', from: accounts[0]});
-  console.log(result.options.address);
-};
+
+
+const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode }) // add bytecode
+    .send({ from: accounts[0] });
+
+    console.log('contract deployed to: ',  result.options.address);
+}
 deploy();
+
