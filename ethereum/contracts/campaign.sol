824dbfca607cc9749bf7d44327cd18737754bd9c
pardeep-mern <pardeep889@hotmail.com>
2020-09-18 12:47:44 +0530

    redepoly contract

--- ethereum/contracts/campaign.sol
+++ ethereum/contracts/campaign.sol
@@ -2,21 +2,18 @@ pragma solidity ^0.4.17;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
-    
-    function createCampaign (uint256 minimum) public {
+
+    function createCampaign(uint256 minimum) public {
         address newCampaign = new Compaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
-    
+
     function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
     }
-    
 }
 
-
 contract Compaign {
-    
     //  members declarations
     struct Request {
         string description;
@@ -26,58 +23,87 @@ contract Compaign {
         uint256 approvalCount;
         mapping(address => bool) approvals;
     }
-    
+
     Request[] public reqeusts;
     address public manager;
     uint256 public minimumContribution;
     mapping(address => bool) public approvers;
     uint256 public approversCount;
-    
-    // modifiers 
+
+    // modifiers
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
+
     // constructors
     constructor(uint256 minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
+
     // member functions
-    
+
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
         approversCount++;
     }
-    
-    function createRequest(string description, uint256 value, address recipient) public restricted {
-      Request memory newRequest = Request({
-         description: description,
-         value: value,
-         recipient: recipient,  
-         complete: false,
-         approvalCount: 0
-       });
-       reqeusts.push(newRequest);
+
+    function createRequest(
+        string description,
+        uint256 value,
+        address recipient
+    ) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        reqeusts.push(newRequest);
     }
-    
+
     function approveRequest(uint256 index) public {
         Request storage reqeust = reqeusts[index];
         require(approvers[msg.sender]);
         require(!reqeust.approvals[msg.sender]);
-        
+
         reqeust.approvals[msg.sender] = true;
         reqeust.approvalCount++;
-        
     }
-    
-    function finalizeRequest(uint256 index) public restricted{
+
+    function finalizeRequest(uint256 index) public restricted {
         Request storage reqeust = reqeusts[index];
-        require(reqeust.approvalCount > (approversCount / 2 ));
+        require(reqeust.approvalCount > (approversCount / 2));
         require(!reqeust.complete);
-        
+
         reqeust.recipient.transfer(reqeust.value);
         reqeust.complete = true;
+    }
+
+    function getSummary()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        return (
+            minimumContribution,
+            address(this).balance,
+            reqeusts.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestCount() public view returns (uint256) {
+        return reqeusts.length;
     }
 }
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from './web';
 import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), 
-    '0xa0B690D33F1a2a2760B7fE57b1658145810e467D'
+    '0x5f293006C080788B9d19fc0cCDFcc9c29D3ac5aA'
 );
 
 export default instance;
